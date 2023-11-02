12ef472cc3bd41a98300ebb1efb80d6a2e0a5087
Piotr Godzień <piotrgodzien@gmail.com>
2023-11-02 10:12:42 +0100

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
         <>
             <section className="py-12 mt-8 bg-primary">
-                <div className="container flex flex-col items-center text-center md:text-left md:flex-row space-y-8 md:space-y-0 justify-evenly px-4 mx-auto max-w-7xl text-white font-bold text-xl">
+                <div className="container flex flex-col items-center text-center md:text-left md:flex-row space-y-8 md:space-y-0 justify-between px-4 mx-auto max-w-7xl text-white font-bold text-xl">
                     <div className="">
                         <p className="mb-3">Przemyśl</p>
                         <p className="text-base font-normal">Grunwaldzka 5, 37-700 Przemyśl</p>
@@ -24,6 +24,9 @@ export default function Footer() {
                         <p className="text-base font-normal">+48 572 327 761</p>
                         <p className="text-base font-normal">(17) 852 79 81</p>
                     </div>
+                </div>
+                <div className="container mx-auto max-w-7xl px-4 mt-4 border-t">
+                    <div className="mt-4 text-center text-white">Stworzono przez <a href="https://getpromo.pl/strony-internetowe/" target='blank' className="text-secondary">Strony internetowe Getpromo</a></div>
                 </div>
             </section>
         </>
